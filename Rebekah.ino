60b46bcd788b87747374a33f253c334c703fc572
SDMesaRoboticsOrganization <sdmesa.roboticsorganization@gmail.com>
2017-04-27 14:38:39 -0700

    File Added
    Code for Rebekah. Front and back had four rows of five NeoPixels each for a total of forty NeoPixels. Lights will turn all white, sparkle white and blue randomly, turn all blue, sparkle white and blue randomly, then turn white and blue chasing.

---
+++ Rebekah.ino
@@ -0,0 +1,114 @@
+//#include <Wire.h>
+
+#include <Adafruit_NeoPixel.h>
+
+// Parameter 1 = number of pixels in strip
+// Parameter 2 = pin number (most are valid)
+// Parameter 3 = pixel type flags, add together as needed:
+//   NEO_RGB     Pixels are wired for RGB bitstream
+//   NEO_GRB     Pixels are wired for GRB bitstream
+//   NEO_KHZ400  400 KHz bitstream (e.g. FLORA pixels)
+//   NEO_KHZ800  800 KHz bitstream (e.g. High Density LED strip)
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(40, 1, NEO_GRB + NEO_KHZ800);
+
+// Here is where you can put in your favorite colors that will appear!
+// just add new {nnn, nnn, nnn}, lines. They will be picked out randomly
+//                                  R   G   B
+uint8_t myFavoriteColors[][3] = {{200, 200, 200},
+                                 {0, 0, 200},
+                                 {200, 200, 200}};
+// don't edit the line below
+#define FAVCOLORS sizeof(myFavoriteColors) / 3
+
+void setup() 
+{
+  strip.begin();
+  strip.show(); // Initialize all pixels to 'off'
+}
+
+void loop() 
+{
+  colorWipe(strip.Color(0, 0, 200), 50);  //set all to white
+  flashRandom(100, 10);  // first number is 'wait' delay, shorter num == shorter twinkle
+  colorWipe(strip.Color(200, 200, 200), 50);  //set all to blue
+  flashRandom(100, 10);  // first number is 'wait' delay, shorter num == shorter twinkle
+  theaterChase(strip.Color(200, 200, 200), strip.Color(0, 0, 200), 50);  //white and blue
+}
+
+
+
+void flashRandom(int wait, uint8_t howmany) {
+
+  for(uint16_t i=0; i<howmany; i++) 
+  {
+    // pick a random favorite color!
+    int c = random(FAVCOLORS);
+    int red = myFavoriteColors[c][0];
+    int green = myFavoriteColors[c][1];
+    int blue = myFavoriteColors[c][2]; 
+
+    // get a random pixel from the list
+    int j = random(strip.numPixels());
+    //Serial.print("Lighting up "); Serial.println(j); 
+    
+    // now we will 'fade' it in 5 steps
+    for (int x=2; x < 5; x++) 
+    {
+      int r = red * (x+1); r /= 5;
+      int g = green * (x+1); g /= 5;
+      int b = blue * (x+1); b /= 5;
+      
+      strip.setPixelColor(j, strip.Color(r, g, b));
+      strip.setPixelColor(j+4, strip.Color(r, g, b));
+      strip.show();
+      delay(wait);
+    }
+    // & fade out in 5 steps
+    for (int x=5; x >= 2; x--) 
+    {
+      int r = red * x; r /= 5;
+      int g = green * x; g /= 5;
+      int b = blue * x; b /= 5;
+      
+      strip.setPixelColor(j, strip.Color(r, g, b));
+      strip.setPixelColor(j+4, strip.Color(r, g, b));
+      strip.show();
+      delay(wait);
+    }
+  }
+}
+
+
+void colorWipe(uint32_t c, uint8_t wait) 
+{
+  for(uint16_t i=0; i<strip.numPixels(); i++) 
+  {
+    strip.setPixelColor(i, c);
+    strip.show();
+    delay(wait);
+  }
+}
+
+void theaterChase(uint32_t c, uint32_t d, uint8_t wait) 
+{
+  for (int j=0; j<10; j++) 
+  {  //do 10 cycles of chasing
+    for (int q=0; q < 3; q++) 
+    {
+      for (uint16_t i=0; i < strip.numPixels(); i=i+3) 
+      {
+        strip.setPixelColor(i+q, c);    //turn every third pixel to white
+      }
+      strip.show();
+
+      delay(wait);
+
+      for (uint16_t i=0; i < strip.numPixels(); i=i+3) 
+      {
+        strip.setPixelColor(i+q, d);        //turn every third pixel to blue
+      }
+    }
+  }
+}
+
+
