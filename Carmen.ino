be60285158a42fecc4cc3e9f57059cc7e17e5047
SDMesaRoboticsOrganization <sdmesa.roboticsorganization@gmail.com>
2017-05-09 12:27:22 -0700

    Update Carmen.ino
    Cleaned up code, comments added.

--- Carmen.ino
+++ Carmen.ino
@@ -1,25 +1,27 @@
+//Author: Anthony Garcia
+//Date: 5/9/17
+//Project: Carmen - 2017 Golden Scissors Fashion Show
+//Version: 1.2
+
 int brightness = 0;    // how bright the LED is
 int fadeAmount = 5;    // how many points to fade the LED by
-int counter = 0;       // counter to keep track of cycles
+int counter = 0;       // counter to cycle through pins
 
-// the setup routine runs once when you press reset:
+
 void setup()
 {
-  // declare pins to be an outputs:
+  // declare all pins to be an outputs for Gemma
   pinMode(0, OUTPUT);
   pinMode(1, OUTPUT);
   pinMode(2, OUTPUT);
 }
 
-// the loop routine runs over and over again forever:
 void loop()
 {
-  // set the brightness of the analog-connected LEDs:
+  // set the brightness of the LEDs one group at a time
   analogWrite(counter, brightness);
-  //analogWrite(1, brightness);
-  //analogWrite(2, brightness);
 
-  // change the brightness for next time through the loop:
+  // change the brightness for next time through the loop
   brightness = brightness + fadeAmount;
 
   // reverse the direction of the fading at the ends of the fade:
@@ -27,17 +29,20 @@ void loop()
   {
     fadeAmount = -fadeAmount;
   }
+  
+  // if end of fade, change to different output pin
   if (brightness == 0)
   {
+    // if not on last pin increment
     if (counter < 2)
     {
       counter++;
     }
-    else
+    else  // if on last pin, reset to pin 0
     {
       counter = 0;
     }
   }
-  // wait for 15 milliseconds to see the dimming effect
+
   delay(15);
 }
